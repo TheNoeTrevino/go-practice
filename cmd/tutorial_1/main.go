9771d41d63112bc323df90aa614e0b4ad7fe4df0
TheNoeTrevino <noetrevino.music@gmail.com>
2024-11-04 01:04:26 -0600

    add: arrays
    - fixed size

--- cmd/tutorial_1/main.go
+++ cmd/tutorial_1/main.go
@@ -1,29 +1,11 @@
 package main
 
 import (
-	"errors"
 	"fmt"
 )
 
 func main() {
-	number1 := 5
-	number2 := 0
-	result, remainder, err := intDivision(number1, number2)
-
-	if err != nil {
-		fmt.Println(err)
-	} else {
-		fmt.Printf("the result is %v, and the remainder is %v", result, remainder)
-	}
-}
-
-func intDivision(numerator int, denominator int) (int, int, error) {
-	var err error
-	if denominator == 0 {
-		err = errors.New("the denominator cannot be 0")
-		return 0, 0, err
-	}
-	result := numerator / denominator
-	remainder := numerator % denominator
-	return result, remainder, err
+	var myNum int32 = 520
+	intArr := [3]int32{1, 2, myNum}
+	fmt.Printf("our array is: %v", intArr)
 }
