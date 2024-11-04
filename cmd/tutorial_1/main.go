919479bab38be4fa3c8e563a024799b0f234e890
TheNoeTrevino <noetrevino.music@gmail.com>
2024-11-04 03:43:01 -0600

    start: hashmap

--- cmd/tutorial_1/main.go
+++ cmd/tutorial_1/main.go
@@ -6,8 +6,27 @@ import (
 
 func main() {
 	intSlice := []int32{1, 2, 3}
-	fmt.Printf("our array is: %v", intSlice)
+	fmt.Printf("our array is: %v\n", intSlice)
 
 	intSlice = append(intSlice, 4)
-	fmt.Printf("our array after slicing: %v", intSlice)
+	fmt.Printf("our array after slicing: %v\n", intSlice)
+
+	myMap := map[string]uint8{"noe": 24}
+	myMap["josh"] = 4
+	fmt.Printf("Map: %v\n", myMap)
+	counter := 0
+	for name, age := range myMap {
+		counter += 1
+		fmt.Printf("ID: %v, Name: %v, Age: %v\n", counter, name, age)
+	}
+
+	for i := 0; i < 10; i++ {
+		fmt.Println("hello")
+	}
 }
+
+func print() {
+	fmt.Println("hello")
+}
+
+const noe = value
