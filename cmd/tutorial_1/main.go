898e6d2d85b85bcac583338b3e8f558fb1c7b75a
TheNoeTrevino <noetrevino.music@gmail.com>
2024-11-04 01:14:51 -0600

    add: slices
    - slices can grow and shrink in size

--- cmd/tutorial_1/main.go
+++ cmd/tutorial_1/main.go
@@ -5,7 +5,9 @@ import (
 )
 
 func main() {
-	var myNum int32 = 520
-	intArr := [3]int32{1, 2, myNum}
-	fmt.Printf("our array is: %v", intArr)
+	intSlice := []int32{1, 2, 3}
+	fmt.Printf("our array is: %v", intSlice)
+
+	intSlice = append(intSlice, 4)
+	fmt.Printf("our array after slicing: %v", intSlice)
 }
